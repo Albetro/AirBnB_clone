62de1fa1727a7cc5c0d62050e55f341b4f7e01c8
Albetro <albertxang@gmail.com>
2023-08-14 00:05:46 +0300

    AirBnB console

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Documentation for the class amenity"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Defines the Amenity class"""
+    name = ""
